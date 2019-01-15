c889ebf27c66d2d147293d4f9c29ef9d475ff69d
n.bidari <n.bidari@samsung.com>
2019-01-14 22:45:58 -0800

    cleaned formatting

--- supermonk-services/supermonk-springboot-app/src/main/java/com/supermonk/cloud/constants/ConstantsSwagger.java
+++ supermonk-services/supermonk-springboot-app/src/main/java/com/supermonk/cloud/constants/ConstantsSwagger.java
@@ -1,133 +1,69 @@
 package com.supermonk.cloud.constants;
 
 public class ConstantsSwagger {
-
-	public static final java.lang.String SWAGGER_OK_TEXT = "OK";
-
-	public static final java.lang.String SWAGGER_CREATED_TEXT = "Created";
-
-	public static final java.lang.String SWAGGER_ACCEPTED_TEXT = "Accepted";
-
-	public static final java.lang.String SWAGGER_NO_CONTENT_TEXT = "No Content";
-
-	public static final java.lang.String SWAGGER_MOVED_PERMANENTLY_TEXT = "Moved Permanently";
-
-	public static final java.lang.String SWAGGER_MOVED_TEMPORARILY_TEXT = "Moved Temporarily";
-
-	public static final java.lang.String SWAGGER_NOT_MODIFIED_TEXT = "Not Modified";
-
-	public static final java.lang.String SWAGGER_BAD_REQUEST_TEXT = "Bad Request";
-
-	public static final java.lang.String SWAGGER_UNAUTHORIZED_TEXT = "Unauthorized";
-
-	public static final java.lang.String SWAGGER_FORBIDDEN_TEXT = "Forbidden";
-
-	public static final java.lang.String SWAGGER_NOT_FOUND_TEXT = "Not Found";
-
-	public static final java.lang.String SWAGGER_INTERNAL_SERVER_ERROR_TEXT = "Internal Server Error";
-
-	public static final java.lang.String SWAGGER_NOT_IMPLEMENTED_TEXT = "Not Implemented";
-
-	public static final java.lang.String SWAGGER_BAD_GATEWAY_TEXT = "Bad Gateway";
-
-	public static final java.lang.String SWAGGER_SERVICE_UNAVAILABLE_TEXT = "Service Unavailable";
-
-	public static final java.lang.String SWAGGER_HEADER = "HEADER";
-
+	public static final String SWAGGER_OK_TEXT = "OK";
+	public static final String SWAGGER_CREATED_TEXT = "Created";
+	public static final String SWAGGER_ACCEPTED_TEXT = "Accepted";
+	public static final String SWAGGER_NO_CONTENT_TEXT = "No Content";
+	public static final String SWAGGER_MOVED_PERMANENTLY_TEXT = "Moved Permanently";
+	public static final String SWAGGER_MOVED_TEMPORARILY_TEXT = "Moved Temporarily";
+	public static final String SWAGGER_NOT_MODIFIED_TEXT = "Not Modified";
+	public static final String SWAGGER_BAD_REQUEST_TEXT = "Bad Request";
+	public static final String SWAGGER_UNAUTHORIZED_TEXT = "Unauthorized";
+	public static final String SWAGGER_FORBIDDEN_TEXT = "Forbidden";
+	public static final String SWAGGER_NOT_FOUND_TEXT = "Not Found";
+	public static final String SWAGGER_INTERNAL_SERVER_ERROR_TEXT = "Internal Server Error";
+	public static final String SWAGGER_NOT_IMPLEMENTED_TEXT = "Not Implemented";
+	public static final String SWAGGER_BAD_GATEWAY_TEXT = "Bad Gateway";
+	public static final String SWAGGER_SERVICE_UNAVAILABLE_TEXT = "Service Unavailable";
+	public static final String SWAGGER_HEADER = "HEADER";
 	public static final int SWAGGER_CONTINUE = 100;
-
 	public static final int SWAGGER_SWITCHING_PROTOCOLS = 101;
-
 	public static final int SWAGGER_PROCESSING = 102;
-
 	public static final int SWAGGER_OK = 200;
-
 	public static final int SWAGGER_CREATED = 201;
-
 	public static final int SWAGGER_ACCEPTED = 202;
-
 	public static final int SWAGGER_NON_AUTHORITATIVE_INFORMATION = 203;
-
 	public static final int SWAGGER_NO_CONTENT = 204;
-
 	public static final int SWAGGER_RESET_CONTENT = 205;
-
 	public static final int SWAGGER_PARTIAL_CONTENT = 206;
-
 	public static final int SWAGGER_MULTI_STATUS = 207;
-
 	public static final int SWAGGER_MULTIPLE_CHOICES = 300;
-
 	public static final int SWAGGER_MOVED_PERMANENTLY = 301;
-
 	public static final int SWAGGER_MOVED_TEMPORARILY = 302;
-
 	public static final int SWAGGER_SEE_OTHER = 303;
-
 	public static final int SWAGGER_NOT_MODIFIED = 304;
-
 	public static final int SWAGGER_USE_PROXY = 305;
-
 	public static final int SWAGGER_TEMPORARY_REDIRECT = 307;
-
 	public static final int SWAGGER_BAD_REQUEST = 400;
-
 	public static final int SWAGGER_UNAUTHORIZED = 401;
-
 	public static final int SWAGGER_PAYMENT_REQUIRED = 402;
-
 	public static final int SWAGGER_FORBIDDEN = 403;
-
 	public static final int SWAGGER_NOT_FOUND = 404;
-
 	public static final int SWAGGER_METHOD_NOT_ALLOWED = 405;
-
 	public static final int SWAGGER_NOT_ACCEPTABLE = 406;
-
 	public static final int SWAGGER_PROXY_AUTHENTICATION_REQUIRED = 407;
-
 	public static final int SWAGGER_REQUEST_TIMEOUT = 408;
-
 	public static final int SWAGGER_CONFLICT = 409;
-
 	public static final int SWAGGER_GONE = 410;
-
 	public static final int SWAGGER_LENGTH_REQUIRED = 411;
-
 	public static final int SWAGGER_PRECONDITION_FAILED = 412;
-
 	public static final int SWAGGER_REQUEST_TOO_LONG = 413;
-
 	public static final int SWAGGER_REQUEST_URI_TOO_LONG = 414;
-
 	public static final int SWAGGER_UNSUPPORTED_MEDIA_TYPE = 415;
-
 	public static final int SWAGGER_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
-
 	public static final int SWAGGER_EXPECTATION_FAILED = 417;
-
 	public static final int SWAGGER_INSUFFICIENT_SPACE_ON_RESOURCE = 419;
-
 	public static final int SWAGGER_METHOD_FAILURE = 420;
-
 	public static final int SWAGGER_UNPROCESSABLE_ENTITY = 422;
-
 	public static final int SWAGGER_LOCKED = 423;
-
 	public static final int SWAGGER_FAILED_DEPENDENCY = 424;
-
 	public static final int SWAGGER_INTERNAL_SERVER_ERROR = 500;
-
 	public static final int SWAGGER_NOT_IMPLEMENTED = 501;
-
 	public static final int SWAGGER_BAD_GATEWAY = 502;
-
 	public static final int SWAGGER_SERVICE_UNAVAILABLE = 503;
-
 	public static final int SWAGGER_GATEWAY_TIMEOUT = 504;
-
 	public static final int SWAGGER_HTTP_VERSION_NOT_SUPPORTED = 505;
-
 	public static final int SWAGGER_INSUFFICIENT_STORAGE = 507;
 
 }
